d9244152a947dce1b2dcf368ac9a2e89ca6bbc94
Wojciech Default <wojciechklanowski@gmail.com>
2020-06-02 01:33:29 +0200

    Planner dev

--- Planner/Windows/PlannerWindow.xaml.cs
+++ Planner/Windows/PlannerWindow.xaml.cs
@@ -1,21 +1,12 @@
 ﻿using Planner.Utils;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using DataGridCell = System.Windows.Controls.DataGridCell;
 using MessageBox = System.Windows.Forms.MessageBox;
 
